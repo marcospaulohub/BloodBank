3d6043baaa0ab82a685f04109308f18b5703394e
Marcos Paulo <marcos.poo@gmail.com>
2025-05-22 05:00:48 -0300

    feat: Criando DbContext e DoadorMap.

---
+++ BloodBank.Infra/BloodBankDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BloodBank.Core.Entities;
+using BloodBank.Infra.Persistence.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBank.Infra
+{
+    public class BloodBankDbContext : DbContext
+    {
+        public BloodBankDbContext(DbContextOptions<BloodBankDbContext> options)
+            : base(options) { }
+        
+        public DbSet<Doador> Doadores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new DoadorMap());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ BloodBank.Infra/Persistence/Mappings/DoadorMap.cs
@@ -0,0 +1,74 @@
+﻿using BloodBank.Core.Entities;
+using BloodBank.Core.ValueObject;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodBank.Infra.Persistence.Mappings
+{
+    public class DoadorMap : IEntityTypeConfiguration<Doador>
+    {
+        public void Configure(EntityTypeBuilder<Doador> builder)
+        {
+            builder.ToTable("Doadores");
+
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.NomeCompleto)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            builder.Property(d => d.Email)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+
+            builder.Property(d => d.DataNascimento)
+                .HasColumnType("date")
+                .IsRequired();
+
+            builder.Property(d => d.Sexo)
+                .HasConversion<int>()
+                .IsRequired();
+
+            builder.Property(d => d.Peso)
+                .HasColumnType("decimal(5,2)")
+                .IsRequired();
+
+            builder.Property(d => d.FatorRh)
+                .HasConversion(
+                    v => v.Simbolo, // grava como "+" ou "-"
+                    v => FatorRh.APartir(v)!) // leitura (com ! assumindo valor válido)
+                .HasColumnType("char(1)")
+                .IsRequired();
+
+            builder.Property(d => d.TipoSanguineo)
+                .HasConversion(
+                    v => v.Valor, // grava como "A+", "B-", etc.
+                    v => TipoSanguineo.APartir(v)!)
+                .HasColumnType("varchar(3)")
+                .IsRequired();
+
+            builder.OwnsOne(d => d.Endereco, endereco =>
+            {
+                endereco.Property(e => e.Logradouro)
+                    .HasColumnType("varchar(100)")
+                    .HasColumnName("Logradouro")
+                    .IsRequired();
+
+                endereco.Property(e => e.Cidade)
+                    .HasColumnType("varchar(100)")
+                    .HasColumnName("Cidade")
+                    .IsRequired();
+
+                endereco.Property(e => e.Estado)
+                    .HasColumnType("char(2)")
+                    .HasColumnName("Estado")
+                    .IsRequired();
+
+                endereco.Property(e => e.Cep)
+                    .HasColumnType("varchar(9)") // considerando "00000-000"
+                    .HasColumnName("Cep")
+                    .IsRequired();
+            });
+        }
+    }
+}
