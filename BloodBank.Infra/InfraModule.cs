dbbe4ac4d4fbebba706a51449da3b86704c9453f
Marcos Paulo <marcos.poo@gmail.com>
2025-05-23 03:49:38 -0300

    feat: Criação do InfraModule.

---
+++ BloodBank.Infra/InfraModule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BloodBank.Infra
+{
+    public static class InfraModule
+    {
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddData(configuration);
+
+            return services;
+        }
+        private static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("BloodBankCs");
+            services.AddDbContext<BloodBankDbContext>(o => o.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
